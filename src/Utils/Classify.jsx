587a22e0b25d98039fe32829dcc392efb878695c
Caner Türkmen <canerturkmen@msn.com>
2022-03-18 01:27:19 +0300

    deployment bug fix

--- src/Utils/Classify.jsx
+++ src/Utils/Classify.jsx
@@ -3,7 +3,6 @@ import '@tensorflow/tfjs';
 
 const classify = async (text) => {
   const threshold = 0.9;
-  let sentences = [];
   const model = await toxicity.load(threshold);
   const predictions = await model.classify(text);
   
