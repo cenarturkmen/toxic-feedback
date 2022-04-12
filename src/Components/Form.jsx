dd83eccf64ce5699d7516eaa80df1d996fbfecc8
Caner Türkmen <canerturkmen@msn.com>
2022-04-12 21:27:36 +0300

    bug fix

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -2,7 +2,7 @@ const Form = (props) => {
   return (
     <form className="flex flex-col w-72 content-center justify-center  ">
       <input
-        className="rounded   bg-stone-500  text-white   border-0 p-2 m-2  hover:border-0"
+        className="rounded  mx-0 bg-stone-500  text-white   border-0 p-2 m-2  hover:border-0"
         onChange={props.handleFormChange}
         name="name"
         type="mail"
@@ -10,7 +10,7 @@ const Form = (props) => {
         placeholder="Your name"
       />
       <input
-        className="rounded  bg-stone-500  text-white   border-0 p-2 m-2  hover:border-0"
+        className="rounded mx-0 bg-stone-500  text-white   border-0 p-2 m-2  hover:border-0"
         onChange={props.handleFormChange}
         name="email"
         type="text"
@@ -18,14 +18,14 @@ const Form = (props) => {
       />
       <textarea
         required
-        className="rounded  bg-stone-500  text-white   h-28 border-0 p-2 m-2  focus:b-0"
+        className="rounded  bg-stone-500  mx-0 text-white   h-28 border-0 p-2 m-2  focus:b-0"
         onChange={props.handleFormChange}
         name="message"
         placeholder=" Your Message"
       ></textarea>
       <button
         onClick={props.handleSend}
-        className="rounded  bg-stone-600   text-white   border-0 p-2 m-2 hover:bg-stone-700 hover:border-0"
+        className="rounded  bg-stone-600 mx-0  text-white   border-0 p-2 m-2 hover:bg-stone-700 hover:border-0"
       >
         Submit
       </button>
